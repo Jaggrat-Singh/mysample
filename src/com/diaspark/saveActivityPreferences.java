3555c22d2f1d4b3d6f86d8acc0187a37e17dd568
jagrat <jaggratsingh@gmail.com>
2011-08-26 14:25:35 +0530

    init

---
+++ src/com/diaspark/saveActivityPreferences.java
@@ -0,0 +1,5 @@
+package com.diaspark;
+
+public @interface saveActivityPreferences {
+
+}
---
+++ src/com/diaspark/test.java
@@ -0,0 +1,33 @@
+package com.diaspark;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class test extends Activity {
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button bt = (Button)findViewById(R.id.add);
+        bt.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast toast = Toast.makeText(getApplicationContext(), "Choose '<WALLPAPER NAME>' from the list to start the Live Wallpaper.",Toast.LENGTH_LONG);
+		        toast.show();
+
+		        Intent intent = new Intent();
+		        intent.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
+		        startActivity(intent);
+
+				
+			}
+		});
+        
+    }
+}
