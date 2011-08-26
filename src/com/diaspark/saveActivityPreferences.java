98e69045ca054baaadce38ab2d1658cda3e4b512
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-26 20:21:26 +0530

    new commit

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -4,12 +4,14 @@ public @interface saveActivityPreferences {
 	
 	
 	// yes i have done some changes 
-
+//asaasasa
 <<<<<<< HEAD
 	
 	//more changs 
 //asasdas
+//JAGRAT
 // more chages
+//asdhasdasd
 =======
  	
 >>>>>>> 6cd3e4b2a9cc8ee9e31e30710a0dfb6b4cbcbda3
