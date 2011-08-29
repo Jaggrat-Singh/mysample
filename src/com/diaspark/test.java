bc6d4179277b4d39705a7521d66524a78d1dbfee
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-29 18:09:51 +0530

    now to 4b85

--- src/com/diaspark/test.java
+++ src/com/diaspark/test.java
@@ -20,7 +20,8 @@ public class test extends Activity {
 			public void onClick(View v) {
 				Toast toast = Toast.makeText(getApplicationContext(), "Choose '<WALLPAPER NAME>' from the list to start the Live Wallpaper.",Toast.LENGTH_LONG);
 		        toast.show();
-
+				//after chnaging in the project...
+		        Toast.makeText(getApplicationContext(), "sasa", 1000).show();
 		        Intent intent = new Intent();
 		        intent.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
 		        startActivity(intent);
