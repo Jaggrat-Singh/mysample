a418612fff5f8d2966a2cfd8a56debf5891c5fe8
Jaggrat Singh <jaggratsingh@gmail.com>
2011-08-30 17:05:46 +0530

    Edited src/com/diaspark/saveActivityPreferences.java via GitHub

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -2,19 +2,5 @@ package com.diaspark;
 
 public interface saveActivityPreferences {
 	
-	
-	// yes i have done some changes 
-//asaasasa
-	//1
-	//12
-<<<<<<< HEAD
-	
-	//more changs 
-//asasdas
-//JAGRAT
-// more chages
-//asdhasdasd
-=======
- 	
->>>>>>> 6cd3e4b2a9cc8ee9e31e30710a0dfb6b4cbcbda3
+// branch  JAGRAT only
 }
