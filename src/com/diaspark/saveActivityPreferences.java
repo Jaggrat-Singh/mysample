57260c026b09d47d93e5c5d83ffb40f0c597f667
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-30 16:01:35 +0530

    MERGESSS

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -1,8 +1,9 @@
 package com.diaspark;
 
 public interface saveActivityPreferences {
-
+	//MASTER
 	// i have started smart git s/w
 	// this line is added frm the website...letsc if this is refelected back to the eclipse
 	// some more chnages  frm the eclipse editor .....
+	
 }
