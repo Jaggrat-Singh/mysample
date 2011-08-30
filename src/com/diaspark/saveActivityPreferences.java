9021f03ff3b4ccc79703d984a3cf57554e845a21
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-30 15:26:37 +0530

    after undoooo frm the master

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -4,4 +4,5 @@ public interface saveActivityPreferences {
 
 	// i have started smart git s/w
 	// this line is added frm the website...letsc if this is refelected back to the eclipse
+	// some more chnages  frm the eclipse editor .....
 }
