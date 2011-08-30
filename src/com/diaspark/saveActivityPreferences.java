7d1fd1ff554c2316dffd33df03502bc0bcfbae50
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-30 15:16:39 +0530

    frm smart git

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -1,20 +1,7 @@
 package com.diaspark;
 
 public interface saveActivityPreferences {
+
+	// i have started smart git s/w
 	
-	
-	// yes i have done some changes 
-//asaasasa
-	//1
-	//12
-<<<<<<< HEAD
-	
-	//more changs 
-//asasdas
-//JAGRAT
-// more chages
-//asdhasdasd
-=======
- 	
->>>>>>> 6cd3e4b2a9cc8ee9e31e30710a0dfb6b4cbcbda3
 }
