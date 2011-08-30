66579bb0f2b44188e74875c6ed3a512e549a8de4
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-30 17:53:27 +0530

    undooo

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -9,6 +9,11 @@ public interface saveActivityPreferences {
 	
 	// THESE CHANGES SHOULD GO IN THE JAGRAT BARNCH]]]] 
 	// some more changes frm the editor .....lets c the jagrat]]
+<<<<<<< HEAD
 
 >>>>>>> 436c9f99053a633b73d3780bc00497e2bff761c9
+=======
+	// lets ccccccc
+	// this is from thw website	
+>>>>>>> 2fe4fb4200eb4412f17a0bfd6465af150ddabf91
 }
