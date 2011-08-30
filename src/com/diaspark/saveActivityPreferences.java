132d8a44900b55635607d88c0e1669658a4b41ce
Jaggrat Singh <jaggratsingh@gmail.com>
2011-08-30 15:15:47 +0530

    i have add some line from website

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -3,5 +3,5 @@ package com.diaspark;
 public interface saveActivityPreferences {
 
 	// i have started smart git s/w
-	
+	// this line is added frm the website...letsc if this is refelected back to the eclipse
 }
