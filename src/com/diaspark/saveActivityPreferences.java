f673f46a712a1d0a558546b04fcb1a6c6c880fe5
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-30 15:48:04 +0530

    is this chnages go in the jagrat]]] branch

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -3,18 +3,7 @@ package com.diaspark;
 public interface saveActivityPreferences {
 	
 	
-	// yes i have done some changes 
-//asaasasa
-	//1
-	//12
-<<<<<<< HEAD
+	// THESE CHANGES SHOULD GO IN THE JAGRAT BARNCH]]]] 
 	
-	//more changs 
-//asasdas
-//JAGRAT
-// more chages
-//asdhasdasd
-=======
- 	
->>>>>>> 6cd3e4b2a9cc8ee9e31e30710a0dfb6b4cbcbda3
+
 }
