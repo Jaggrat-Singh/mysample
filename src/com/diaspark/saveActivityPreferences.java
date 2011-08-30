d18f7fa6fd373cb10fc8f5d0670ce2aeabefeffe
Jaggrat-Singh <jaggratsingh@gmail.com>
2011-08-30 16:24:53 +0530

    commited fmr cmd

--- src/com/diaspark/saveActivityPreferences.java
+++ src/com/diaspark/saveActivityPreferences.java
@@ -6,4 +6,11 @@ public interface saveActivityPreferences {
 	// this line is added frm the website...letsc if this is refelected back to the eclipse
 	// some more chnages  frm the eclipse editor .....
 	
+<<<<<<< HEAD
+=======
+	
+	// THESE CHANGES SHOULD GO IN THE JAGRAT BARNCH]]]] 
+	// some more changes frm the editor .....lets c the jagrat]]
+
+>>>>>>> ade5224e3184906860d55a7db17cdda701549960
 }
